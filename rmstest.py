82944102b5d6f84e10ae55f3b062b104c6cce7c6
kabilkumarkas <155209586+kabilkumarkas@users.noreply.github.com>
2024-01-01 11:25:16 +0530

    Update rmstest.py

--- rmstest.py
+++ rmstest.py
@@ -1,16 +1,12 @@
 import streamlit as st
-import pandas as pd
 
+import streamlit as st
+import pandas as pd
 from pdfminer.high_level import extract_text
 
 import sqlite3
-import io
+
 import re
-import string
-import os
-import numpy as np
-import datetime
-import uuid
 def fetch_data():
     connection = sqlite3.connect("recruitment_management_system.db")  # Replace with your database file name
     query = "SELECT * FROM your_table"  # Replace with your table name
@@ -22,11 +18,19 @@ def fetch_data():
 def display_table(data):
     # Use st.dataframe to display a Pandas DataFrame
     st.dataframe(data)
-# Define the main function
-def main():
-    # Create a title for the web application
-    st.title("Recruitment Management System")
+def intro():
+    st.write("# Welcome to Streamlit! 👋")
+    st.sidebar.success("Select a demo above.")
 
+    st.markdown("""
+     # This is test """)
+
+def cinput():
+
+    st.title(":blue[Recruit Management System]")
+    # Create a title for the web application
+    st.markdown("""
+            This is input page """)
     # Create a form to upload a resume
     st.subheader("Upload a Resume")
     uploaded_file = st.file_uploader("Upload your resume here", type="pdf")
@@ -38,7 +42,7 @@ def main():
         # Parse the extracted text to identify personal information
         name = re.findall(r"^[A-Za-z]*(?: [A-Za-z]+)*", text)
         email = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}", text)
-        phone_number = re.findall(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]',text)
+        phone_number = re.findall(r'[\+\(]?[1-9][0-9 .\-\(\)]{8,}[0-9]', text)
         skills = re.findall(r"(?<!\w)([A-Za-z]+)(?!\w)", text)
         education = re.findall(r"([A-Za-z]+) ([A-Za-z]+) ([A-Za-z]+) ([A-Za-z]+)", text)
         experience = re.findall(r"(?<!\w)([A-Za-z]+)(?!\w)", text)
@@ -53,23 +57,17 @@ def main():
             "experience": experience
         }
 
-        # Display the extracted information on the web application
-        #st.subheader("Extracted Resume Information")
-        #st.write(resume_data)
-        #resume_data = pd.DataFrame(resume_data)
-
-        # Save the extracted information to a CSV file
-        #with open("resume_data.csv", "w") as f:
-           # f.write(resume_data.to_csv())
-
         # Create a form to add a candidate to the database
         st.subheader("Add a Candidate")
-        candidate_name = st.text_input("Enter the candidate's name", key="candidate_name1", value=resume_data.get("name", ""))
+        candidate_name = st.text_input("Enter the candidate's name", key="candidate_name1",
+                                       value=resume_data.get("name", "").title())
         candidate_email = st.text_input("Enter the candidate's email", value=resume_data.get("email", ""))
-        candidate_phone_number = st.text_input("Enter the candidate's phone number",value=resume_data.get("phone_number", ""))
-        candidate_skills = st.text_input("Enter the candidate's skills",value=resume_data.get("skills", ""))
+        candidate_phone_number = st.text_input("Enter the candidate's phone number",
+                                               value=resume_data.get("phone_number", ""))
+        candidate_skills = st.text_input("Enter the candidate's skills", value=resume_data.get("skills", ""))
         candidate_education = st.text_input("Enter the candidate's education", value=resume_data.get("education", ""))
-        candidate_experience = st.text_input("Enter the candidate's experience", value=resume_data.get("experiance", ""))
+        candidate_experience = st.text_input("Enter the candidate's experience",
+                                             value=resume_data.get("experiance", ""))
 
     # Create a button to add the candidate to the database
     if st.button("Add Candidate"):
@@ -78,7 +76,10 @@ def main():
         cursor = connection.cursor()
 
         # Insert the candidate's information into the database
-        cursor.execute("INSERT INTO candidates (name, email, phone_number, skills, education, experience) VALUES (?, ?, ?, ?, ?, ?)", (candidate_name, candidate_email, candidate_phone_number, candidate_skills, candidate_education, candidate_experience))
+        cursor.execute(
+            "INSERT INTO candidates (name, email, phone_number, skills, education, experience) VALUES (?, ?, ?, ?, ?, ?)",
+            (candidate_name, candidate_email, candidate_phone_number, candidate_skills, candidate_education,
+             candidate_experience))
 
         # Commit the changes to the database
         connection.commit()
@@ -89,68 +90,73 @@ def main():
         # Display a message to the user
         st.success("Candidate added to the database")
 
-    # Create a form to search for a candidate
-    st.subheader("Search for a Candidate")
-    candidate_name = st.text_input("Enter the candidate's name",key="candidate_name2")
 
-    # Create a button to search for the candidate
-    if st.button("Search"):
-        # Create a database connection
-        connection = sqlite3.connect("recruitment_management_system.db")
-        cursor = connection.cursor()
 
-        # Select the candidate's information from the database
-        cursor.execute("SELECT * FROM candidates WHERE name = ?", (candidate_name,))
 
-        # Fetch the candidate's information from the database
-        candidate_data = cursor.fetchone()
+def cupdate():
+    st.write("# Recruit Management System")
+    st.markdown("""
+        This is Search page """)
+    # Connect to SQLite database
+    conn = sqlite3.connect('recruitment_management_system.db')
 
-        # Close the database connection
-        connection.close()
+    # Read SQL query into a DataFrame
+    df = pd.read_sql_query("SELECT * FROM candidates", conn)
 
-        # Display the candidate's information on the web application
-        st.subheader("Candidate Information")
-        #st.write(candidate_data)
-        st.dataframe(candidate_data)
-    # Create a form to generate a report
-    st.subheader("Generate a Report")
-    report_type = st.selectbox("Select the type of report", ["All Candidates", "Candidates by Skill", "Candidates by Education", "Candidates by Experience"])
+    # Show dataframe in streamlit
+    edited_df = st.data_editor(df)
 
-    # Create a button to generate the report
-    if st.button("Generate Report"):
-        # Create a database connection
-        connection = sqlite3.connect("recruitment_management_system.db")  # Replace with your database file name
-        query = "SELECT * FROM candidates"  # Replace with your table name
-        data = pd.read_sql_query(query, connection)
-        connection.close()
-        #connection = sqlite3.connect("recruitment_management_system.db")
-        #cursor = connection.cursor()
+    # If dataframe is edited, update the SQLite database
+    if not edited_df.equals(df):
+        edited_df.to_sql('candidates', conn, if_exists='replace', index=False)
 
-        #data = fetch_data()
+def creports():
 
-        # Display data table
-        display_table(data)
-        # Generate the report based on the selected report type
+    st.title(":blue[Recruit Management System]")
+    st.markdown("""
+        Report page """)
+    connection = sqlite3.connect("recruitment_management_system.db")  # Replace with your database file name
+    query = "SELECT * FROM candidates"  # Replace with your table name
+    data = pd.read_sql_query(query, connection)
+    connection.close()
+    # connection = sqlite3.connect("recruitment_management_system.db")
+    # cursor = connection.cursor()
 
-        #
-    if st.button("Filter Report"):
-        connection = sqlite3.connect("recruitment_management_system.db")  # Replace with your database file name
-        query = "SELECT * FROM candidates"  # Replace with your table name
-        data = pd.read_sql_query(query, connection)
-        connection.close()
-        # Display the report on the web application
-        #st.subheader("Report")
-        # Add a filter to the data table
-        filter_column = st.selectbox("Select a column for filtering", data.columns)
-        filter_value = st.text_input(f"Enter {filter_column} value to filter")
+    # data = fetch_data()
 
-        # Apply the filter to the data
-        filtered_data = data[data[filter_column] == filter_value]
+    # Display data table
+    display_table(data)
 
-        # Display the filtered data
-        st.subheader("Filtered Data")
-        st.dataframe(filtered_data)
-        #st.write(report_data)
-        #st.dataframe(report_data)
-if __name__ == "__main__":
-    main()
+# st.set_page_config(
+#     page_title="Ex-stream-ly Cool App",
+#     page_icon="🧊",
+#     layout="wide",
+#     initial_sidebar_state="expanded",
+#     menu_items={
+#         'Get Help': 'https://www.extremelycoolapp.com/help',
+#         'Report a bug': "https://www.extremelycoolapp.com/bug",
+#         'About': "# This is a header. This is an *extremely* cool app!"
+#     }
+# )
+#
+# page_names_to_funcs = {
+#     "—": intro,
+#     "Add Candidate": cinput,
+#     "Update": cupdate,
+#     "Report": creports
+# }
+
+#demo_name = st.sidebar("Choose a demo", page_names_to_funcs.keys())
+#page_names_to_funcs[demo_name]()
+pages = {
+    "Home": intro,
+    "Add Candidate": cinput,
+     "Update": cupdate,
+     "Report": creports
+}
+
+# Create a sidebar with navigation links
+selection = st.sidebar.radio("Go to", list(pages.keys()))
+
+# Display the selected page
+pages[selection]()
